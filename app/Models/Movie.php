ad5de1c8ea131eeb7a39d01918b20376f52bc99e
Ehsan Shahbakhsh <ehsan.shahbakhsh.email@gmail.com>
2026-08-07 01:35:03 +0330

    feat(slug): publish config and implement sluggable in Movie model

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -6,11 +6,13 @@ use App\Enums\MovieStatus;
 use Database\Factories\MovieFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Movie extends Model
 {
     /** @use HasFactory<MovieFactory> */
     use HasFactory;
+    use Sluggable;
 
     protected $fillable = [
         'title',
@@ -29,4 +31,18 @@ class Movie extends Model
         'duration_minutes' => 'integer',
         'status' => MovieStatus::class,
     ];
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
 }
