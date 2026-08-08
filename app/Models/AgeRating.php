9ea1d360b9284b8eb21b43fa0e62c66530b4d5b0
Ehsan Shahbakhsh <ehsan.shahbakhsh.email@gmail.com>
2026-08-08 11:31:21 +0330

    feat(age_rating): add movies relationship

--- app/Models/AgeRating.php
+++ app/Models/AgeRating.php
@@ -6,10 +6,16 @@ use Database\Factories\AgeRatingFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 #[Fillable(['name'])]
 class AgeRating extends Model
 {
     /** @use HasFactory<AgeRatingFactory> */
     use HasFactory;
+
+    public function movies(): HasMany
+    {
+        return $this->hasMany(Movie::class);
+    }
 }
