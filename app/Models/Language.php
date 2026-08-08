026ce0f4d1c13ce7d53878a300112835c41b633b
Ehsan Shahbakhsh <ehsan.shahbakhsh.email@gmail.com>
2026-08-08 17:59:29 +0330

    feat(language): add Language model, migration, and factory

---
+++ app/Models/Language.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\LanguageFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Language extends Model
+{
+    /** @use HasFactory<LanguageFactory> */
+    use HasFactory;
+
+    public $timestamps = false;
+}
