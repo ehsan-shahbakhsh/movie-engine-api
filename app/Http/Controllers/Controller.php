67a6b459c949977094d050cfcd506a1052b289ae
Ehsan Shahbakhsh <ehsan.shahbakhsh.email@gmail.com>
2026-08-12 19:04:17 +0330

    docs(api): add initial OpenAPI info to base Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use OpenApi\Attributes as OA;
+
+#[OA\Info(
+    version: "1.0.0",
+    description: "Official API documentation for the Movie Engine backend architecture.",
+    title: "Movie Engine API",
+)]
 abstract class Controller
 {
     //
