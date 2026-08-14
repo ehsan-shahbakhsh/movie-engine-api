5c046d22b5501531e34c8263ed22a8a74064877a
Ehsan Shahbakhsh <ehsan.shahbakhsh.email@gmail.com>
2026-08-14 23:47:02 +0330

    feat(movie): add videos relationship to Movie model

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -14,6 +14,7 @@ use Illuminate\Database\Eloquent\Model;
 use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
@@ -78,6 +79,11 @@ class Movie extends Model implements HasMedia
             ->using(MoviePerson::class)
             ->withPivot(['department', 'job', 'character_name'])
             ->withTimestamps();
+    }
+
+    public function videos(): MorphMany
+    {
+        return $this->morphMany(Video::class, 'videoable');
     }
 
     public function registerMediaCollections(): void
