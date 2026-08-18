522bcd8c4256c06c35b1d8925f9837b998b0053c
Ehsan Shahbakhsh <ehsan.shahbakhsh.email@gmail.com>
2026-08-18 09:17:26 +0330

    chore(season): add API resource attribute to Season model

--- app/Models/Season.php
+++ app/Models/Season.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use App\Http\Resources\Api\V1\SeasonResource;
 use Database\Factories\SeasonFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\UseResource;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+#[UseResource(SeasonResource::class)]
 #[Fillable(['series_id', 'season_number', 'title', 'release_date', 'end_date'])]
 class Season extends Model
 {
