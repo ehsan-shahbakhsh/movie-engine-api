7c38e6d77212a38f6db9a6d1efc33da2de549e02
Ehsan Shahbakhsh <ehsan.shahbakhsh.email@gmail.com>
2026-08-19 16:57:21 +0330

    feat(download_link): add video media collection to DownloadLink model

--- app/Models/DownloadLink.php
+++ app/Models/DownloadLink.php
@@ -7,12 +7,15 @@ use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
 #[Fillable(['download_group_id', 'quality_id', 'encoder_id', 'codec_id'])]
-class DownloadLink extends Model
+class DownloadLink extends Model implements HasMedia
 {
     /** @use HasFactory<DownloadLinkFactory> */
     use HasFactory;
+    use InteractsWithMedia;
 
     public function downloadGroup(): BelongsTo
     {
@@ -32,5 +35,21 @@ class DownloadLink extends Model
     public function codec(): BelongsTo
     {
         return $this->belongsTo(Codec::class);
+    }
+
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection('video')
+            ->singleFile()
+            ->useDisk('public')
+            ->acceptsMimeTypes([
+                'video/mp4',
+                'video/webm',
+                'video/ogg',
+                'video/quicktime',
+                'video/x-msvideo',
+                'video/x-matroska',
+                'video/x-flv',
+            ]);
     }
 }
