7c5aadadb388df9a723e06a37abfed4aa9c24666
Ehsan Shahbakhsh <ehsan.shahbakhsh.email@gmail.com>
2026-08-23 21:07:06 +0330

    chore(favorite): add API resource attributes to Favorite model

--- app/Models/Favorite.php
+++ app/Models/Favorite.php
@@ -2,13 +2,19 @@
 
 namespace App\Models;
 
+use App\Http\Resources\Api\V1\FavoriteCollection;
+use App\Http\Resources\Api\V1\FavoriteResource;
 use Database\Factories\FavoriteFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\UseResource;
+use Illuminate\Database\Eloquent\Attributes\UseResourceCollection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 
+#[UseResource(FavoriteResource::class)]
+#[UseResourceCollection(FavoriteCollection::class)]
 #[Fillable(['user_id', 'favoritable_id', 'favoritable_type'])]
 class Favorite extends Model
 {
