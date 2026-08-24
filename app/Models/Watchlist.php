de03169e558e5658e996d9ecea295e8c9d9754e4
Ehsan Shahbakhsh <ehsan.shahbakhsh.email@gmail.com>
2026-08-25 01:30:39 +0330

    chore(watchlist): add API resource attributes to Watchlist model

--- app/Models/Watchlist.php
+++ app/Models/Watchlist.php
@@ -2,13 +2,19 @@
 
 namespace App\Models;
 
+use App\Http\Resources\Api\V1\WatchlistCollection;
+use App\Http\Resources\Api\V1\WatchlistResource;
 use Database\Factories\WatchlistFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\UseResource;
+use Illuminate\Database\Eloquent\Attributes\UseResourceCollection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 
+#[UseResource(WatchlistResource::class)]
+#[UseResourceCollection(WatchlistCollection::class)]
 #[Fillable(['user_id', 'watchable_id', 'watchable_type'])]
 class Watchlist extends Model
 {
