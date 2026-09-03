aa2e901d15197b73fbf5835920d859f286c6f30a
Ehsan Shahbakhsh <ehsan.shahbakhsh.email@gmail.com>
2026-09-03 15:16:43 +0330

    test(series): provide required pivot data for persons relationship

--- tests/Feature/Api/V1/SeriesTest.php
+++ tests/Feature/Api/V1/SeriesTest.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Models\{Series, AgeRating, Language, Genre, Person, Video, Season, Episode, DownloadGroup, DownloadLink};
-use App\Enums\SeriesPublishStatus;
+use App\Enums\{SeriesPublishStatus, PersonSeriesDepartment};
 use Illuminate\Database\Eloquent\Factories\Sequence;
 
 it('returns a paginated list of series', function () {
@@ -185,7 +185,11 @@ it('returns series details correctly by slug', function () {
         ->for(AgeRating::factory()->state(['name' => 'PG-13']), 'ageRating')
         ->for(Language::factory()->state(['name' => 'Persian', 'code' => 'fa']), 'originalLanguage')
         ->hasAttached(Genre::factory()->count(3), relationship: 'genres')
-        ->hasAttached(Person::factory()->count(3), relationship: 'persons')
+        ->hasAttached(
+            Person::factory()->count(3),
+            ['department' => PersonSeriesDepartment::Acting],
+            relationship: 'persons',
+        )
         ->has(Video::factory()->count(3), 'videos')
         ->has(
             Season::factory()
